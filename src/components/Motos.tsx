de2105f7653c656626ff7735cfacba9971d7abc8
Milton Raúl Vergara García <elmotigre6@gmail.com>
2024-06-20 04:45:57 -0500

    se reparó los items del grid de motos en el listado de datos del componente Motos

--- src/components/Motos.tsx
+++ src/components/Motos.tsx
@@ -8,27 +8,26 @@ type Motoslist = {
 }
 
 export default function Motos({ datamotos }: { datamotos: Array<Motoslist> }) {
-  console.log(datamotos);
-  
+
   return (
-    <>
+
+    <div className="motos-container">
       {
         datamotos.map((moto) => (
-          <div className="motos-container" key={moto.id}>
-            <div className="motos_section">
-              <img src={"/images/"+moto.img} alt="" className='motos_img' />
-              <div className="details-motos">
-                <div>
-                  <h3>{moto.name}</h3>
-                </div>
-                <p className='text_motos'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae odit fuga illo cumque quis doloremque aspernatur expedita ducimus facere deserunt temporibus culpa aperiam veritatis, earum minus explicabo, doloribus nostrum voluptates!</p>
-                <button>Agregar al Carrito</button>
+          <div className="motos_section" key={moto.id}>
+            <img src={"/images/" + moto.img} alt="" className='motos_img' />
+            <div className="details-motos">
+              <div>
+                <h3>{moto.name}</h3>
               </div>
+              <p className='text_motos'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae odit fuga illo cumque quis doloremque aspernatur expedita ducimus facere deserunt temporibus culpa aperiam veritatis, earum minus explicabo, doloribus nostrum voluptates!</p>
+              <button>Agregar al Carrito</button>
             </div>
           </div>
         ))
       }
-    </>
+    </div>
+
 
 
   )
