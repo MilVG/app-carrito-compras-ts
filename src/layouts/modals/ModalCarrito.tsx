9b1d7241f31cf9da57c6b0e491f42b064ec9b834
Milton Raúl Vergara García <87380726+MilVG@users.noreply.github.com>
2024-08-24 00:27:17 -0500

    Actualización: dispatchi hook  useReducer
    Se agregaron a cada elemento las acciones correspondientes del dispatch.

--- src/layouts/modals/ModalCarrito.tsx
+++ src/layouts/modals/ModalCarrito.tsx
@@ -1,14 +1,13 @@
+import { useMemo } from 'react'
 import { cartProps } from '../../types/types.ts'
-export default function ModalCarrito(
-  {carrito,isFlex,totalCart,handleIncrementQuantity,handleDescrementQuantity,handleDeleteItem,handleclearCart
-  }:cartProps
-) {
+export default function ModalCarrito({ cart, isFlex, dispatch }: cartProps) {
 
-  
+  const totalCart = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.precio), 0), [cart])
+
   return (
     <div className='modal-carrito' style={{ display: isFlex ? 'flex' : 'none' }}>
 
-      <h2 className='title-carrito'> {carrito.length > 0 ? 'TIENES ' + carrito.length + ' ITEMS' : 'El carrito esta Vacio'}</h2>
+      <h2 className='title-carrito'> {cart.length > 0 ? 'TIENES ' + cart.length + ' ITEMS' : 'El carrito esta Vacio'}</h2>
 
       <table className='table-carrito'>
         <thead>
@@ -21,19 +20,19 @@ export default function ModalCarrito(
           </tr>
         </thead>
         <tbody>
-          {carrito.map((moto) => (
+          {cart.map((moto) => (
 
             <tr key={moto.id}>
               <td><img src={`/images/${moto.img}`} alt="" width={40} height={40} style={{ borderRadius: '50%' }} /></td>
               <td>{moto.name}</td>
               <td>{moto.precio}</td>
               <td>
-                <button onClick={() => handleDescrementQuantity(moto.id)}>-</button>
+                <button onClick={() => dispatch({ type: 'reducir-cantidad-item', payload: { id: moto.id } })}>-</button>
                 {moto.quantity}
-                <button onClick={() => handleIncrementQuantity(moto.id)}>+</button>
+                <button onClick={() => dispatch({ type: 'aumentar-cantidad-item', payload: { id: moto.id } })}>+</button>
               </td>
               <td>
-                <button onClick={() => handleDeleteItem(moto.id)} style={{ color: '#ff5733', borderRadius: '50%', cursor: 'pointer' }}>X</button>
+                <button onClick={() => dispatch({ type: 'eliminar-item-carrito', payload: { id: moto.id } })} style={{ color: '#ff5733', borderRadius: '50%', cursor: 'pointer' }}>X</button>
               </td>
             </tr>
           ))
@@ -46,7 +45,7 @@ export default function ModalCarrito(
             <td>S/.{totalCart}</td>
           </tr>
           <tr>
-            <td><button className='btn-clear-carrito' onClick={() => handleclearCart()}>VACIAR CARRITO</button></td>
+            <td><button className='btn-clear-carrito' onClick={() => dispatch({ type: 'vaciar-carrito' })}>VACIAR CARRITO</button></td>
           </tr>
         </tfoot>
       </table>
