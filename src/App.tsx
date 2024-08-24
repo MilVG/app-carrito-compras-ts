f40059a48845cacb60eb56b0d45dacd34de5293c
Milton Raúl Vergara García <87380726+MilVG@users.noreply.github.com>
2024-08-24 00:34:55 -0500

    Actualización: prorps nuevos y dispatch
    se modificó la mayoria de props y se reemplazó por el manejador del hook
    useReducer mediante las acciones definidas.

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,31 @@ import Footer from './layouts/Footer'
 import Main from './layouts/Main'
 import './styles/mainaside_style.css'
 import Carrito from './components/Carrito'
-import useCart from './hooks/usecart'
-import { useReducer } from 'react'
+import { useEffect, useReducer } from 'react'
 import { carritoReducer, initialState } from './reducers/cart-reducer'
+import DisplayShowCart from './hooks/DisplayShowCart'
 function App() {
-  const { handleshowCarrito, isFlex, setCarrito, carrito, datamotos, handleToCarrito, totalCart, handleclearCart, handleDeleteItem, handleIncrementQuantity, handleDescrementQuantity } = useCart()
+
+  const { handleshowCarrito, isFlex } = DisplayShowCart()
 
   const [state, dispatch] = useReducer(carritoReducer, initialState)
+
+  useEffect(() => {
+    localStorage.setItem('carrito', JSON.stringify(state.cart))
+  }, [state.cart])
   return (
     <>
       <Header />
       <Carrito
-        carrito={carrito}
-        setCarrito={setCarrito}
+        cart={state.cart}
         handleshowCarrito={handleshowCarrito}
         isFlex={isFlex}
-        totalCart={totalCart}
-        handleIncrementQuantity={handleIncrementQuantity}
-        handleDescrementQuantity={handleDescrementQuantity}
-        handleDeleteItem={handleDeleteItem}
-        handleclearCart={handleclearCart}
+        dispatch={dispatch}
       />
       <div className='container_main' >
         <Main
-          datamotos={datamotos}
-          handleToCarrito={handleToCarrito}
+          datamotos={state.data}
+          dispatch={dispatch}
         />
       </div>
       <Footer />
