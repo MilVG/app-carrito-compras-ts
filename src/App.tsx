0c29be7033e29b55a27fd5bcedf94a128c57264a
Milton Raúl Vergara García <87380726+MilVG@users.noreply.github.com>
2024-08-24 00:09:29 -0500

    actulización: hookCurstom a useReducer
    se cambiaron las diferntes funciones independientes a un un manejador
    especifico como use Reducer.

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,27 @@ import Main from './layouts/Main'
 import './styles/mainaside_style.css'
 import Carrito from './components/Carrito'
 import useCart from './hooks/usecart'
+import { useReducer } from 'react'
+import { carritoReducer, initialState } from './reducers/cart-reducer'
 function App() {
-  const {handleshowCarrito,isFlex,setCarrito,carrito,datamotos,handleToCarrito,totalCart,handleclearCart,handleDeleteItem,handleIncrementQuantity,handleDescrementQuantity} = useCart()
+  const { handleshowCarrito, isFlex, setCarrito, carrito, datamotos, handleToCarrito, totalCart, handleclearCart, handleDeleteItem, handleIncrementQuantity, handleDescrementQuantity } = useCart()
+
+  const [state, dispatch] = useReducer(carritoReducer, initialState)
   return (
     <>
       <Header />
       <Carrito
-        carrito = {carrito}
-        setCarrito = {setCarrito}
-        handleshowCarrito = {handleshowCarrito}
-        isFlex = {isFlex}
+        carrito={carrito}
+        setCarrito={setCarrito}
+        handleshowCarrito={handleshowCarrito}
+        isFlex={isFlex}
         totalCart={totalCart}
         handleIncrementQuantity={handleIncrementQuantity}
         handleDescrementQuantity={handleDescrementQuantity}
         handleDeleteItem={handleDeleteItem}
         handleclearCart={handleclearCart}
       />
-      <div  className='container_main' >
+      <div className='container_main' >
         <Main
           datamotos={datamotos}
           handleToCarrito={handleToCarrito}
--- src/components/Carrito.tsx
+++ src/components/Carrito.tsx
@@ -2,29 +2,29 @@ import ModalCarrito from '../layouts/modals/ModalCarrito';
 import '../styles/carrito_style.css'
 import { GiShoppingCart } from "react-icons/gi";
 import { cartProps } from '../types/types';
-export default function Carrito( {carrito,setCarrito,handleshowCarrito,isFlex,totalCart,handleIncrementQuantity,handleDescrementQuantity,handleDeleteItem,handleclearCart} :  cartProps ) {
+export default function Carrito({
+  cart,
+  handleshowCarrito,
+  isFlex,
+  dispatch,
+}: cartProps) {
   return (
-<>
-    <ModalCarrito
-      carrito={carrito}
-      isFlex={isFlex}
-      setCarrito={setCarrito}
-      handleshowCarrito={handleshowCarrito}
-      totalCart={totalCart}
-      handleIncrementQuantity={handleIncrementQuantity}
-      handleDescrementQuantity={handleDescrementQuantity}
-      handleDeleteItem={handleDeleteItem}
-      handleclearCart={handleclearCart}
-    />
-    <div className='pcarrito'>
-      <button className='btn-carrito' onClick={() => handleshowCarrito()}>
-        <div className='cout-item'>
-          {carrito.length}
-        </div>
-        <GiShoppingCart className='icon-carrito' size={50} />
-      </button>
-    </div>
-</>
+    <>
+      <ModalCarrito
+        cart={cart}
+        isFlex={isFlex}
+        handleshowCarrito={handleshowCarrito}
+        dispatch={dispatch}
+      />
+      <div className='pcarrito'>
+        <button className='btn-carrito' onClick={() => handleshowCarrito()}>
+          <div className='cout-item'>
+            {cart.length}
+          </div>
+          <GiShoppingCart className='icon-carrito' size={50} />
+        </button>
+      </div>
+    </>
 
   )
 }
